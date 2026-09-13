e1f053e20c4404d2d81c2f037ea4108d050c40d7
qtrsvln <61349590+qtrsvln@users.noreply.github.com>
2026-09-13 16:52:57 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {base: process.env.GITHUB_ACTIONS ? '/default/' : './',
+  return {base: process.env.GITHUB_ACTIONS ? '/kitchen-companion-2/' : './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
